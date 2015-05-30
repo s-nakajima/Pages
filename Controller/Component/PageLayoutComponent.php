7e9b2f3c3fd5aba5a45d45f4ef9efeb3738296a5
s-nakajima <nakajimashouhei@gmail.com>
2015-05-30 22:30:37 +0900

    Fix reconsidering page

--- Controller/Component/PageLayoutComponent.php
+++ Controller/Component/PageLayoutComponent.php
@@ -37,6 +37,7 @@ class PageLayoutComponent extends Component {
 			$controller->layout = 'Pages.default';
 		}
 
+		//pathからページデータ取得
 		if (isset($controller->viewVars['page'])) {
 			$page = $controller->viewVars['page'];
 		} else {
@@ -54,99 +55,32 @@ class PageLayoutComponent extends Component {
 			}
 			$page = $controller->camelizeKeyRecursive($page);
 		}
-//		$this->_View->set('page', $page);
 
+		//cancelUrlをセット
 		if (! isset($controller->viewVars['cancelUrl'])) {
 			$controller->set('cancelUrl', $page['page']['permalink']);
 		}
-		//$controller->set('path', '/' . $page['permalink']);
 
-
-//		//ページデータ取得
-//		if (isset($this->_View->viewVars['page'])) {
-//			$page = $this->_View->viewVars['page'];
-//		} else {
-//			if (isset($this->_View->viewVars['current'])) {
-//				$path = $this->_View->viewVars['current']['page']['permalink'];
-//			} else {
-//				$path = '';
-//			}
-//
-//			$pageModel = ClassRegistry::init('Pages.Page');
-//			$page = $pageModel->getPageWithFrame($path);
-//			if (empty($page)) {
-//				throw new NotFoundException();
-//			}
-//			$page = NetCommonsAppController::camelizeKeyRecursive($page);
-//			$this->_View->set('page', $page);
-//		}
-//		$this->_View->viewVars['current']['page'] = NetCommonsAppController::camelizeKeyRecursive($page['page']);
-//		$this->_View->viewVars['current']['frame']['roomId'] = $this->_View->viewVars['current']['page']['roomId'];
-//		$this->_View->viewVars['current']['frame']['languageId'] = $page['language'][0]['id'];
-//		$this->_View->set('frame', $this->_View->viewVars['current']['frame']);
-//
-//		if (! isset($this->_View->viewVars['cancelUrl'])) {
-//			$this->_View->set('cancelUrl', $this->_View->viewVars['current']['page']['permalink']);
-//		}
-//		$this->_View->set('path', '/' . $this->_View->viewVars['current']['page']['permalink']);
-//
-//		$this->__containers = Hash::combine($page['container'], '{n}.type', '{n}');
-//		$this->__boxes = Hash::combine($page['box'], '{n}.id', '{n}', '{n}.containerId');
-//
-//		//プラグインデータ取得
-//		if (! isset($this->_View->viewVars['plugins'])) {
-//			$pluginsRoom = ClassRegistry::init('Rooms.PluginsRoom');
-//			$plugins = $pluginsRoom->getPlugins(
-//				$this->_View->viewVars['current']['frame']['roomId'],
-//				$this->_View->viewVars['current']['frame']['languageId']
-//			);
-//			if (empty($plugins)) {
-//				throw new NotFoundException();
-//			}
-//
-//			$plugins = NetCommonsAppController::camelizeKeyRecursive($plugins);
-//			$pluginMap = Hash::combine($plugins, '{n}.plugin.key', '{n}.plugin');
-//
-//			$this->_View->set('plugins', $plugins);
-//			$this->_View->set('pluginMap', $pluginMap);
-//		}
-
+		//Pluginデータ取得
 		$pluginsRoom = ClassRegistry::init('Rooms.PluginsRoom');
 		$plugins = $pluginsRoom->getPlugins($page['page']['roomId'], $controller->viewVars['languageId']);
 		if (empty($plugins)) {
 			throw new NotFoundException();
 		}
 
-
-//		$pageModel = ClassRegistry::init('Pages.Page');
-//		$path = '';
-//		$page = $pageModel->getPageWithFrame($path);
-//		if (empty($page)) {
-//			throw new NotFoundException();
-//		}
-//
-//		$page['Container'] = Hash::combine($page['Container'], '{n}.type', '{n}');
-//		unset($page['Container'][Container::TYPE_MAIN]);
-//		$page['Box'] = Hash::combine($page['Box'], '{n}.id', '{n}', '{n}.container_id');
-//
-//		$controller->set('containers', $page['Container']);
-//		$controller->set('boxes', $page['Box']);
-//
-//		$controller->layout = 'Pages.default';
-
 		//ページHelper読み込み
 		if (in_array('Pages.PageLayout', $controller->helpers)) {
 			unset($controller->helpers['Pages.PageLayout']);
 		}
+
+		//ページHelperにセット
 		$results = array(
 			'current' => $controller->current,
 			'containers' => Hash::combine($page['container'], '{n}.type', '{n}'),
 			'boxes' => Hash::combine($page['box'], '{n}.id', '{n}', '{n}.containerId'),
 			'plugins' => $controller->camelizeKeyRecursive($plugins),
-//			'pluginMap' => Hash::combine($plugins, '{n}.plugin.key', '{n}.plugin')
 		);
 		$controller->helpers['Pages.PageLayout'] = $controller->camelizeKeyRecursive($results);
-
 	}
 
 }

3a36699784606e90a4a74fcbcd2018ba73f0faf1
s-nakajima <nakajimashouhei@gmail.com>
2016-03-15 18:41:01 +0900

    PagesEditHelper::indent()のテスト

---
+++ Test/Case/View/Helper/PagesEditHelper/IndentTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * PagesEditHelper::indent()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsHelperTestCase', 'NetCommons.TestSuite');
+
+/**
+ * PagesEditHelper::indent()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Pages\Test\Case\View\Helper\PagesEditHelper
+ */
+class PagesEditHelperIndentTest extends NetCommonsHelperTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'pages';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストデータ生成
+		//TODO:必要に応じてセットする
+	}
+
+/**
+ * DataProvider
+ *
+ * ### 戻り値
+ *  - nest インデントのネスト数
+ *
+ * @return array テストデータ
+ */
+	public function dataProvider() {
+		return array(
+			array('nest' => 0),
+			array('nest' => 1),
+			array('nest' => 2),
+			array('nest' => 3),
+		);
+	}
+
+/**
+ * indent()のテスト
+ *
+ * @param int $nest インデントのネスト数
+ * @dataProvider dataProvider
+ * @return void
+ */
+	public function testIndent($nest) {
+		//データ生成
+		$pageId = '2';
+		$parentId = '1';
+
+		//Helperロード
+		$viewVars = array();
+		$viewVars = Hash::insert($viewVars, 'pages.' . $pageId . '.Page.parent_id', $parentId);
+		$viewVars = Hash::insert($viewVars, 'parentList._' . $parentId . '._' . $pageId . '.nest', $nest);
+
+		$requestData = array();
+		$params = array();
+		$this->loadHelper('Pages.PagesEdit', $viewVars, $requestData, $params);
+
+		//テスト実施
+		$result = $this->PagesEdit->indent($pageId);
+
+		//チェック
+		$this->assertEquals($nest, preg_match_all('/' . preg_quote('<span class="pages-tree"> </span>', '/') . '/', $result));
+	}
+
+}

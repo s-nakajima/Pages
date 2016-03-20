d4b58a12957069a2c94728bb9d67a417bf87b198
s-nakajima <nakajimashouhei@gmail.com>
2016-03-20 11:27:11 +0900

    View/Elements/PagesEdit/delete_formのテスト

---
+++ Test/Case/View/Elements/PagesEdit/DeleteFormTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * View/Elements/PagesEdit/delete_formのテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * View/Elements/PagesEdit/delete_formのテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Pages\Test\Case\View\Elements\PagesEdit\DeleteForm
+ */
+class PagesViewElementsPagesEditDeleteFormTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.pages.box4pages',
+		'plugin.pages.boxes_page4pages',
+		'plugin.pages.container4pages',
+		'plugin.pages.containers_page4pages',
+		'plugin.pages.languages_page4pages',
+		'plugin.pages.page4pages',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'pages';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Pages', 'TestPages');
+		//テストコントローラ生成
+		$this->generateNc('TestPages.TestViewElementsPagesEditDeleteForm');
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * View/Elements/PagesEdit/delete_formのテスト
+ *
+ * @return void
+ */
+	public function testDeleteForm() {
+		//テスト実行
+		$this->_testGetAction('/test_pages/test_view_elements_pages_edit_delete_form/edit/2?frame_id=6',
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$pattern = '/' . preg_quote('View/Elements/PagesEdit/delete_form', '/') . '/';
+		$this->assertRegExp($pattern, $this->view);
+
+		$this->assertInput('form', null, '/delete/2/1/7?frame_id=6', $this->view);
+		$this->assertInput('input', '_method', 'DELETE', $this->view);
+		$this->assertInput('input', 'data[Page][id]', '7', $this->view);
+	}
+
+}
---
+++ Test/test_app/Plugin/TestPages/View/TestViewElementsPagesEditDeleteForm/edit.ctp
@@ -0,0 +1,16 @@
+<?php
+/**
+ * View/Elements/PagesEdit/delete_formテスト用Viewファイル
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+?>
+
+View/Elements/PagesEdit/delete_form
+
+<?php echo $this->element('Pages.PagesEdit/delete_form');

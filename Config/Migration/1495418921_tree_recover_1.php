4b2f246ff1dd2b0c037d36ee2d232208c445a0a5
s-nakajima <nakajimashouhei@gmail.com>
2017-05-22 11:22:16 +0900

    ページ移動で内部エラーが発生したため、Migrationでrecoverする https://github.com/NetCommons3/NetCommons3/issues/881

---
+++ Config/Migration/1495418921_tree_recover_1.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * インデックスの見直し
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * インデックスの見直し
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Pages\Config\Migration
+ */
+class TreeRecover1 extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'tree_recover_1';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		$this->loadModels(['Page' => 'Pages.Page']);
+
+		if ($this->connection !== 'test') {
+			$this->Page->setMasterDataSource();
+			$this->Page->recover();
+		}
+		return true;
+	}
+}

797099a49f5d72b222983dabc900aa5510e5317c
s-nakajima <nakajimashouhei@gmail.com>
2017-06-12 10:20:27 +0900

    速度計測

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -51,11 +51,17 @@ class PagesController extends PagesAppController {
  * @return void
  */
 	public function index() {
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'start1 ' . microtime() . '');
+$stime = microtime(true);
+
 		if (Current::isSettingMode() && ! Current::permission('page_editable')) {
 			$paths = $this->params->params['pass'];
 			$path = implode('/', $paths);
 			return $this->redirect('/' . $path);
 		}
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'end1 ' . microtime() . "\t" . (microtime(true) - $stime) . '');
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'start2 ' . microtime() . '');
+$stime = microtime(true);
 
 		//ページデータの取得
 		$paths = $this->params->params['pass'];
@@ -66,12 +72,16 @@ class PagesController extends PagesAppController {
 			'recursive' => -1,
 			'conditions' => array('permalink' => $spacePermalink, 'id !=' => Space::WHOLE_SITE_ID)
 		));
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'end2 ' . microtime() . "\t" . (microtime(true) - $stime) . '');
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'star3 ' . microtime() . '');
+$stime = microtime(true);
 
 		$page = $this->Page->getPageWithFrame($path, $space['Space']['id']);
 		if (empty($page)) {
 			throw new NotFoundException();
 		}
 		$this->set('page', $page);
+CakeLog::debug("\t" . '-- -- -- -- PagesController::' . __FUNCTION__ . "\t" . 'end3 ' . microtime() . "\t" . (microtime(true) - $stime) . '');
 	}
 
 }
